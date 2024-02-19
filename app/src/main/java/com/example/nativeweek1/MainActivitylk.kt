444346adeb83c468370e2056c6e432c98f99899b
dhityarr <dhityarr@gmail.com>
2024-02-19 11:39:49 +0700

    new

--- app/src/main/java/com/example/nativeweek1/MainActivitylk.kt
+++ app/src/main/java/com/example/nativeweek1/MainActivitylk.kt
@@ -3,9 +3,9 @@ package com.example.nativeweek1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivitylk : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_activitylk)
     }
 }
